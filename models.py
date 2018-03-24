3968a014d887e6713226d91c5d736d5889ee3398
Willian Z <Willian@Willian-Zhang.com>
2018-03-24 02:51:02 -0400

    fix style

--- models.py
+++ models.py
@@ -6,14 +6,13 @@ class CreditCard():
     logger = logging.getLogger(__name__)
     app = None
 
-    def __init__(self, name, card, exp, cvv, card_id = None):
+    def __init__(self, name, card, exp, cvv, card_id=None):
         # type: (string, string, (number, number), number, number) -> None
         self.name = name
         self.card = card
         self.exp = exp
         self.cvv = cvv
         self.card_id = card_id
-
 
     @staticmethod
     def init_db(app):
