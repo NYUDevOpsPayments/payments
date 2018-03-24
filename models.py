102dabe2cbfe6e75f8740836baf1bf477e1e901b
Willian Z <Willian@Willian-Zhang.com>
2018-03-24 01:48:40 -0400

    add model

---
+++ models.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import logging
+
+class CreditCard():
+    logger = logging.getLogger(__name__)
+    app = None
+
+    @staticmethod
+    def init_db(app):
+        CreditCard.app = app
+
+        CreditCard.client = MongoClient(app.config['DB_URI']) # type: pymongo.MongoClient
+        CreditCard.db = CreditCard.client[app.config['DB_NAME']] # type: pymongo.collection
+        CreditCard.collection = CreditCard.db['card'] # type: pymongo.collection
+        
+
